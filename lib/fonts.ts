efdfe8f058ec62931e4eeff8d5a3e46aa3a8c62d
dlaffey1 <davidlaffey83@gmail.com>
2025-02-04 16:09:23 +0000

    fixed fallback font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,19 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from 'next/font/google'
+import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans'
-})
+  subsets: ["latin"],
+  variable: "--font-sans",
+  /** 
+   * Disables build-time fetching of the font,
+   * so you won't get ETIMEDOUT on Vercel builds.
+   */
+  preload: false,
+  fallback: ["sans-serif"],
+});
 
 export const fontMono = FontMono({
-  subsets: ['latin'],
-  variable: '--font-mono'
-})
+  subsets: ["latin"],
+  variable: "--font-mono",
+  preload: false,           // Prevent build-time fetch
+  fallback: ["monospace"],  // Fallback if this font fails to load
+});
