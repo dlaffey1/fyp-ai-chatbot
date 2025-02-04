beef0bd282a8c7fd22174e300c7c1c0a91fb1d77
dlaffey1 <davidlaffey83@gmail.com>
2025-02-04 15:49:07 +0000

    Fix lockfile to match package.json

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -27,7 +27,8 @@ export function ChatPanel({
       <ButtonScrollToBottom />
       <div className="mx-auto sm:max-w-2xl sm:px-4">
         {/* 
-          If you want "Stop generating" or "Regenerate response" buttons, you can add them here
+          If you want "Stop generating" or "Regenerate response" buttons,
+          you can add them here in the future
         */}
         <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
--- next.config.js
+++ next.config.js
@@ -2,14 +2,27 @@
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true
+    serverActions: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.githubusercontent.com'
-      }
-    ]
-  }
-}
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Only run this fallback if we're building for the client
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        // Bypass optional native modules
+        bufferutil: false,
+        "utf-8-validate": false,
+      };
+    }
+
+    return config;
+  },
+};
