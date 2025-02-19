d0cff0df0374b38563cd44d5eeb81774b8aece31
dlaffey1 <davidlaffey83@gmail.com>
2025-02-19 12:30:55 +0000

    fixed display error caused by codeblock.tsx

--- components/ui/codeblock.tsx
+++ components/ui/codeblock.tsx
@@ -65,12 +65,8 @@ const CodeBlock: FC<Props> = memo(({ language, value }) => {
       3,
       true
     )}${fileExtension}`
-    const fileName = window.prompt('Enter file name', suggestedFileName)
+    const fileName = window.prompt('Enter file name', suggestedFileName) || suggestedFileName;
 
-    if (!fileName) {
-      // User pressed cancel on prompt.
-      return
-    }
 
     const blob = new Blob([value], { type: 'text/plain' })
     const url = URL.createObjectURL(blob)
